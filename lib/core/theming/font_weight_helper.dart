92bb1cf6922ad2ceedbec0d529093514cfba129b
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-04-17 03:37:24 +0200

    Add core theming classes for colors, font weights, and styles
    Added classes for managing colors, font weights, and text styles in the
    core theming module. This will help maintain consistency and ease of use
    for styling throughout the application.

---
+++ lib/core/theming/font_weight_helper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FontWeightHelper {
+  static const FontWeight thin = FontWeight.w100;
+  static const FontWeight extralight = FontWeight.w200;
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semibold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight extrabold = FontWeight.w800;
+}
