e4d6999b6883de2986dd06c5d5886b24ee144f15
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-04-17 03:36:11 +0200

    Add service locator setup for repositories and cubits
    Initialize service locator with repositories and cubits setup methods
    to register dependencies for the layout feature. This ensures proper
    dependency injection within the application.

---
+++ lib/core/di/service_locator.dart
@@ -0,0 +1,28 @@
+import 'package:get_it/get_it.dart';
+import 'package:spacex/feature/layout/data/repositories/layout_repo.dart';
+import 'package:spacex/feature/layout/data/repositories/layout_repo_impl.dart';
+import 'package:spacex/feature/layout/logic/layout_cubit.dart';
+
+final GetIt serviceLocator = GetIt.instance;
+
+class ServiceLocator {
+  Future<void> setupServiceLocator() async {
+
+    _setupForRepos();
+
+    _setupForCubits();
+  }
+
+  void _setupForRepos() {
+    serviceLocator.registerLazySingleton<LayOutRepo>(() => LayoutRepoImpl());
+
+    
+  }
+
+  void _setupForCubits() {
+
+    serviceLocator.registerFactory<LayoutCubit>(
+        () => LayoutCubit(layOutRepo: serviceLocator.get<LayOutRepo>()));
+   
+  }
+}
