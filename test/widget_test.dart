929df0a9216afe04f46b82bcd0b0c8a89065f1b3
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-04-17 03:38:39 +0200

    Add shared_preferences_foundation plugin and update dependencies
    Added the shared_preferences_foundation plugin for platform-specific
    shared preferences handling. Also updated dependencies for flutter_bloc,
    dio, equatable, flutter_screenutil, get_it, iconsax, and shared_preferences
    to their latest versions. Updated widget_test.dart to use SpceXApp.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:spacex/spacex_app.dart';
 
-import 'package:spacex/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SpceXApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
