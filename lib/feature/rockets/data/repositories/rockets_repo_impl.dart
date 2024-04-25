33d6573ddfc74da05c41e76e8fdb12c2e10d1a4a
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-04-25 04:59:59 +0200

    Add rocket model, repository, logic cubit, and state classes

---
+++ lib/feature/rockets/data/repositories/rockets_repo_impl.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:spacex/core/networking/api_services.dart';
+import 'package:spacex/core/networking/end_boint.dart';
+import 'package:spacex/core/networking/failuer.dart';
+import 'package:spacex/core/networking/servier_failure.dart';
+import 'package:spacex/feature/rockets/data/model/reocket_model.dart';
+import 'package:spacex/feature/rockets/data/repositories/rockets_repo.dart';
+
+class RocketRepoImpl implements RocketRepo {
+  RocketRepoImpl();
+
+  @override
+  Future<Either<Failure, List<RocketModel>>> getRockets() async {
+    try {
+      List<dynamic> response = await ApiServices.getData(endpoint: getAllRocketsendpoint);
+
+      List<RocketModel> rockets =
+          response.map((json) => RocketModel.fromJson(json)).toList();
+
+      return Right(rockets);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
+}
