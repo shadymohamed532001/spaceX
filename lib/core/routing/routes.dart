4eb2c08659e691096e055b09d54f9385109b655b
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-04-25 05:00:28 +0200

    Refactor code formatting in routing and theming files
    Fix inconsistent code formatting in routing and theming files by adjusting
    indentation and removing unnecessary blank lines at the end of files.

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,10 +1,5 @@
 class Routes {
   static const String initialRoute = '/';
   static const String layOutViewsRoute = '/layOutViews';
-<<<<<<< HEAD
-
-    static const String rocketScreenDetailsRoute  = '/rocketScreenDetails';
-
-=======
->>>>>>> 48018df (Refactor code formatting in routing and theming files)
+  static const String rocketScreenDetailsRoute = '/rocketScreenDetails';
 }
