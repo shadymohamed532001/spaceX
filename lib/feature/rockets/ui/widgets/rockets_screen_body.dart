cd586ccc307a4f3d22a1f070fac4ff9cd3f02da1
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-04-25 04:59:45 +0200

    Update RocketsScreenBody to use StatefulWidget and initialize RocketsCubit in initState

--- lib/feature/rockets/ui/widgets/rockets_screen_body.dart
+++ lib/feature/rockets/ui/widgets/rockets_screen_body.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:spacex/feature/rockets/logic/cubit/rockets_cubit.dart';
 import 'package:spacex/feature/rockets/ui/widgets/rocket_launcher_item.dart';
 
-class RocketsScreenBody extends StatelessWidget {
+class RocketsScreenBody extends StatefulWidget {
   const RocketsScreenBody({super.key});
+
+  @override
+  State<RocketsScreenBody> createState() => _RocketsScreenBodyState();
+}
+
+class _RocketsScreenBodyState extends State<RocketsScreenBody> {
+  @override
+  void initState() {
+    BlocProvider.of<RocketsCubit>(context).getRockets();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
