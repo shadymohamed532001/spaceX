39282edc54c02c32bbb36ee63866c313e4d9e7c9
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-05-19 19:59:51 +0300

    nnnnnnnnnn

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,7 +1,10 @@
 class Routes {
   static const String initialRoute = '/';
   static const String layOutViewsRoute = '/layOutViews';
+<<<<<<< HEAD
 
     static const String rocketScreenDetailsRoute  = '/rocketScreenDetails';
 
+=======
+>>>>>>> 48018df (Refactor code formatting in routing and theming files)
 }
